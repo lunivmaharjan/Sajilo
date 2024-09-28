d23891f52f6e492afbbde63da38bbf0335eefbe3
diyach868 <155082615+diyach868@users.noreply.github.com>
2024-09-28 10:08:31 +0545

    Update SigninForm.jsx

--- src/Components/SigninForm/SigninForm.jsx
+++ src/Components/SigninForm/SigninForm.jsx
@@ -5,7 +5,7 @@ const SigninForm = () => {
   return (
     <div className='wrapper'>
         <form action="">
-            <h1>Sign In</h1>
+            <h1>Register</h1>
             <div className='input-box'>
                 <input type="text" placeholder='First Name' required/>
             </div>
@@ -28,7 +28,7 @@ const SigninForm = () => {
                 <label><input type="checkbox" />I agree to all terms and policies</label>
             </div>
 
-            <button type="submit">Sign In</button>
+            <button type="submit">Register</button>
         </form>
     </div>
   )
